c89729969100e003387e44c3c2f6c46d0f9fd376
silviawolves <silviacanepa.w@gmail.com>
2022-05-19 13:21:26 +0200

    codice base

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Il programma dovrà mostrare una form da compilare con cui chiedere all’utente:
+
+nome e cognome
+km da percorrere
+eta (input testuale o number)
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+
+Il recap dei dati e l’output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo)
+
+Al click sul tasto GENERA si genera il biglietto con i calcoli dovuti
+Al click sul tasto ANNULLA si svuotano i campi
+*/
